5815512138fda664ed55443e6e7601c2d5c68863
Martin Tappa <martin.tappa@gmail.com>
2026-06-04 12:57:07 -0300

    mi primer upload de myOwn

---
+++ src/main/java/com/nocta/myown/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.nocta.myown.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+	
+	@GetMapping("/health")
+	public String health(){
+		return "My Own api funcionando";
+	}
+
+}
---
+++ src/test/java/com/nocta/myown/MyownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocta.myown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
