5ab146276c9a9b69c3f9edae4e45de13dcd2227a
Martin Tappa <martin.tappa@gmail.com>
2026-07-23 16:22:30 -0300

    se cambian los datos para que tome apellido2

--- src/main/java/com/nocta/myown/entity/EspecialidadPendiente.java
+++ src/main/java/com/nocta/myown/entity/EspecialidadPendiente.java
@@ -2,6 +2,8 @@ package com.nocta.myown.entity;
 
 import java.time.LocalDateTime;
 
+import com.nocta.myown.enums.EstadoEspecialidadPendiente;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
